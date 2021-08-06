4a061686957ee1524e4326efbf6623a2bef06696
milanm98 <marjanovmilan13@gmail.com>
2021-08-06 23:33:19 +0200

    background

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Background from "./assets/background.jpg";
 
 function App() {
   return (
-    <div>
-     Keli
-    </div>
+    <main className="w-screen h-screen typography">
+        <div className="w-full h-auto text-center text-xs md:text-xl">Modified kelly criterion algorithm by Marjanov Milan</div>
+        <img src={Background} className="background w-full md:w-2/3 h-auto md:h-5/6 flex items-center justify-center" alt="background"></img>
+        <form>
+          <input required type="text" placeholder="Enter odds" className="text-center"></input>
+          <button type="submit">Submit</button>
+        </form>
+        <div className="text-center text-xs md:text-sm">Contact me via email : marjanovmilan12@gmail.com</div>
+    </main>
   );
 }
 
